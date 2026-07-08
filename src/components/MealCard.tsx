a4e6576b99e73b1f4a5491153753f52b2541def0
alessia masuzzo <alessia.masuzzo@edu-its.it>
2026-07-08 23:23:13 +0200

    aggiornato componente card per renderlo responsive in modalità tablet

--- src/components/MealCard.tsx
+++ src/components/MealCard.tsx
@@ -1,5 +1,5 @@
 // components/MealCard.tsx
-import { View, Text, Image, Pressable } from "react-native";
+import { View, Text, Image, Pressable, StyleProp, ViewStyle } from "react-native";
 import { styles } from "../theme/styles";
 import { useFavorites } from "../contex/FavoritesContex";
 import type { MealSummary } from "../types/meal";
@@ -7,14 +7,15 @@ import type { MealSummary } from "../types/meal";
 type Props = {
   meal: MealSummary;
   onPress: (idMeal: string) => void;
+  style?: StyleProp<ViewStyle>;
 };
 
-export function MealCard({ meal, onPress }: Props) {
+export function MealCard({ meal, onPress, style }: Props) {
   const { isFavorite, toggleFavorite } = useFavorites();
   const active = isFavorite(meal.idMeal);
 
   return (
-    <View style={styles.mealCard}>
+    <View style={[styles.mealCard, style]}>
       <Pressable
         style={{ flexDirection: "row", alignItems: "center", flex: 1 }}
         onPress={() => onPress(meal.idMeal)}
